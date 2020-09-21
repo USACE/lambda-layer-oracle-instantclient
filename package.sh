c90f424034abf71a2a49ed0dbed6fed5a9b67542
Brett Palmberg <brett.t.palmberg@usace.army.mil>
2020-09-21 16:29:25 -0500

    additional refactor

--- package.sh
+++ package.sh
@@ -4,17 +4,18 @@
 wget -O instantclient.zip ${INSTANT_CLIENT_DOWNLOAD_URL}
 
 # Make Working Directories
-rm -rf lib tmp; mkdir lib tmp
+rm -rf build tmp; mkdir -p build/bin build/lib tmp
 
 # Unzip Instant Client; -j drops all directory structure, keeps files
 unzip -j instantclient.zip -d ./tmp/
 
-# Change Directory into lib; Copy in all required .so; zip
-cd lib \
-  && cp ../tmp/*.so* ./ \
-  && cp /lib64/libaio.so.1 ./ \
+# Copy in all required .so
+cp /lib64/libaio.so.1 ./tmp/*.so* build/lib/
+
+# Zip it
+cd build \
   && zip -ruq ../${BUILD_PACKAGE_NAME} ./ \
   && cd ..
-
+  
 # Cleanup
-rm -rf lib tmp instantclient.zip
+rm -rf build tmp instantclient.zip
